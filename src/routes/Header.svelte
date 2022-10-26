27378fec39ca8be5c20e87c61ae1d8e5d18cc007
Taylor Lee <52428384+taylorylee@users.noreply.github.com>
2022-10-25 20:03:49 -0700

    created header with nav links and volume function
    using <a> for links to different pages
    
    using <button> or creating a button component for action.
    
    add: sound

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,14 +1,34 @@
 <script>
     import Box from "../components/Box.svelte";
+    import volume_1 from "assets/volume_1.svg";
+    import no_volume from "assets/no_volume.svg";
+    import zap from "assets/zap.svg";
+    import zap_filled from "assets/zap_filled.svg";
+
+    export let audio; 
+
+    function toggleVolume() {
+      audio.play();  
+    }
+
 </script>
 
+<slot name="header"></slot>
+
 <header>
-    <div class="">
-      <a href=""</a></a>  
+    <div class="header">
+      <a href="/Experiments">Experiments</a>
+      <a href="/Writing">Writing</a>
+      <Button {no_volume} on:click={() => {toggleVolume()}}>
+        <img src={volume_1} alt="volume on" />
+      <button href="/static/volume_1.svg" alt="Volume">Music</button> 
+      <button href="/static/zap.svg" alt="Theme Changer">Theme</button>  
     </div>
 </header>
 
-<style>
+<style lang="scss" global>
+    @import "styles/global.scss";
+
     header {
         display: flex;
         box-shadow: $header-shadow; 
