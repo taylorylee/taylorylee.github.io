050c235d9b5d8d1f75da3538f0ed0c383a9d97d0
Taylor Lee <52428384+taylorylee@users.noreply.github.com>
2022-10-27 01:33:38 -0700

    refactor experiment card section to get data from json file
    fix: formatting

--- src/routes/experiments/+page.svelte
+++ src/routes/experiments/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-    import Box from "src/components/Box.svelte";
+    import experiments from 'src/data/Experiments.json';
+    import Card from "src/components/Card.svelte";
 </script>
 
 <svelte:head>
@@ -9,16 +10,18 @@
 <section>
     <h1> Experiments </h1>
     <p> 
-        When I think about experiments, I think about the process of learning and discovery. This page includes projects, work, and other things I've been working on.
+        When I think about experiments, I think about the process of learning and discovery, which includes projects, work, and other things I've been working on.
     </p>
 </section>
 
-
-<Box 
-    title="Testing"
-    bg="hsla(48, 33%, 97%, 1)"
-    category="Work"
-    link=""
-    icon=""
-
-/>
+<div class="card">
+    {#each experiments as experiment}
+        <Card
+            <h5>{experiment.title}</h5>
+            bg={experiment.bg}
+            category={experiment.category}
+            link={experiment.link}
+            icon={experiment.icon}
+        />
+    {/each}
+</div>
