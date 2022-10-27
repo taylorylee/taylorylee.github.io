fb5e4d3ee816d733283850e8a7a52c5fe5d22812
Taylor Lee <52428384+taylorylee@users.noreply.github.com>
2022-10-27 01:10:27 -0700

    custom footer with email and copyright
    adding to the layout and main page

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,5 +1,22 @@
 <script>
-    
+    import mail from "assets/mail.svg";
 </script>
 
-<slot name="footer"></slot>
+<footer>
+    <a class="link" href="mailto:taylorylee@hotmail.com" alt="Email" rel="noopener noreferrer">
+        <img src="{ mail }" />
+    </a>
+    <p> Made by Taylor Lee © 2022 </p>
+</footer>
+
+<style lang="scss">
+
+    footer {
+        margin-top: $y-padding;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        min-height: $footer-min-height;
+    }
+    
+</style>
