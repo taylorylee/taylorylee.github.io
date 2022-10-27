6c4a98823ae24cdac68b9d9a45573b56bc0f16dd
Taylor Lee <52428384+taylorylee@users.noreply.github.com>
2022-10-27 01:22:46 -0700

    renamed Box to Card due to component name fitting. added styling
    due to the structure of svelte components, each component styling will be in respective file

---
+++ src/components/Card.svelte
@@ -0,0 +1,21 @@
+<script>
+	import external from 'assets/external.svg';
+</script>
+
+<div class="box" />
+
+<style lang="scss">
+	@import "styles/global.scss";
+
+	.card {
+		border: 1px dashed $light-green; 
+		border-radius: 10px;
+		background: $white;
+		overflow: hidden;
+		position: absolute;
+		width: 468px;
+		height: 136px;
+		padding: 1em;
+		margin: 0 0 1em 0;
+	}
+</style>
